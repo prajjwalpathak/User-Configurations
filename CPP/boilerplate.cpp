fe534c9935ce7df764dbb4ae95f2c1b4cea85fdf
Prajjwal Pathak <prajjwalpathak1105@gmail.com>
2025-07-01 04:11:34 +0530

    Update boilerplate.cpp

--- CPP/boilerplate.cpp
+++ CPP/boilerplate.cpp
@@ -1,11 +1,7 @@
 #include <bits/stdc++.h>
 #define ll long long
-#define uint unsigned int
-#define co cout <<
-#define ci cin >>
 #define nl cout << "\n"
-#define sz(v) (int)(v.size())
-#define fr(i, a, b) for (int i = (a); i < (b); i++)
+#define for(i, a, b) for (int i = (a); i < (b); i++)
 #define sortasc(v) sort(v.begin(), v.end())                        // sort ascending: O(N log N)
 #define sortdsc(v, type) sort(v.begin(), v.end(), greater<type>()) // sort descending: O(N log N)
 #define mxe(v) *max_element(v.begin(), v.end())                    // find max element in vector: O(N)
@@ -16,11 +12,11 @@ using namespace std;
 void solve()
 {
     int n;
-    ci n;
+    cin >> n;
     vector<int> v(n);
-    fr(i, 0, n)
+    for(i, 0, n)
     {
-        ci v[i];
+        cin >> v[i];
     }
 }
 
@@ -30,7 +26,7 @@ int32_t main()
     cin.tie(NULL);
 
     int t = 1;
-    ci t;
+    cin >> t;
     while (t--)
     {
         solve();
